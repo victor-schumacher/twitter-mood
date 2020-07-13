d0e31132fe2d8df469dbb4315738b6585d4156d0
victor-schumacher <victorschumacher255@gmail.com>
2020-07-13 18:38:35 -0300

    removing idea

--- pkg/data/sentiment.go
+++ pkg/data/sentiment.go
@@ -5,13 +5,17 @@ import (
 	"github.com/cdipaolo/sentiment"
 )
 
-func Analyze(text string){
+func Analyze(text string) bool {
 	model, err := sentiment.Restore()
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	analysis := model.SentimentAnalysis(text, sentiment.English)
-	fmt.Println(analysis.Score)
-
+	if analysis.Score == 1 {
+		return false
+	} else {
+		return true
+	}
 }
+
--- pkg/tw/auth.go
+++ pkg/tw/auth.go
@@ -1,38 +1,21 @@
-package twitter
+package tw
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 )
 
 func Auth() *twitter.Client {
-	// 2zr105kPhmM6qxmkqHeYsl6mC
-	// 7MjZeewsCjLaAZriEr68bJZaFEARmYO1OdrPznXY9sypHWTsVV - secret
-	config := oauth1.NewConfig(os.Getenv("key"), os.Getenv("secretKey"))
+	accessToken:="1130660432681799681-gKk2Qf1LJpuWGGBDhunnCM5TsBQPB9"
+	accessTokenSecret:="7GNSMAnva0V78xCpqdJ433H07zeMbkFERdrYmbeomleH1"
+	key:="2zr105kPhmM6qxmkqHeYsl6mC"
+	secretKey:="7MjZeewsCjLaAZriEr68bJZaFEARmYO1OdrPznXY9sypHWTsVV"
 
-	// 1130660432681799681-gKk2Qf1LJpuWGGBDhunnCM5TsBQPB9
-	// 7GNSMAnva0V78xCpqdJ433H07zeMbkFERdrYmbeomleH1 - secret
-	token := oauth1.NewToken(os.Getenv("accessToken"), os.Getenv("accessTokenSecret"))
 
+	config := oauth1.NewConfig(key, secretKey)
+	token := oauth1.NewToken(accessToken, accessTokenSecret)
 	httpClient := config.Client(oauth1.NoContext, token)
 	client := twitter.NewClient(httpClient)
+
 	return client
-
-	params := twitter.StatusUpdateParams{
-		Status:             "z",
-		InReplyToStatusID:  0,
-		PossiblySensitive:  nil,
-		Lat:                nil,
-		Long:               nil,
-		PlaceID:            "",
-		DisplayCoordinates: nil,
-		TrimUser:           nil,
-		MediaIds:           nil,
-		TweetMode:          "",
-	}
-
-	fmt.Println(params)
 }
